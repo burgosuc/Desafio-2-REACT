10503c57a9806d3272e610a3f00b66aeea690db0
cristobalwalters12 <cristobalwalters@gmail.com>
2023-09-29 21:55:34 -0300

    el componente de alert tiene un fade in

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,9 +1,9 @@
+import "../assets/css/style.css";
+
 const Alert = ({ alerta }) => {
-  const { texto, tipo, estado } = alerta
+  const { texto, tipo, estado } = alerta;
 
-  return (
-    estado && <div className={`alert ${tipo} mt-1`}>{texto}</div>
-  )
-}
+  return estado && <div className={`alert ${tipo} mt-1 fade-in`}>{texto}</div>;
+};
 
-export default Alert
+export default Alert;
